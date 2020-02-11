6c14669c4e4907fb8ff8c4f13a9b31bb24cfea8d
Onur Şahinbay <sahinbayonur@gmail.com>
2020-02-11 14:47:56 +0300

    Initial commit

---
+++ app/src/main/java/com/sahinbay/wordgame/BossAct.java
@@ -0,0 +1,42 @@
+package com.sahinbay.wordgame;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BossAct extends AppCompatActivity {
+
+    TextView textScreen, textQuestion, textTitle, textBtn;
+    ImageView bigboss;
+    Animation smalltobig;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boss);
+
+        smalltobig = AnimationUtils.loadAnimation(this, R.anim.smalltobig);
+
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/FredokaOneRegular.ttf");
+
+        textQuestion = (TextView) findViewById(R.id.textQuestion);
+        textScreen = (TextView) findViewById(R.id.textScreen);
+        textTitle = (TextView) findViewById(R.id.textTitle);
+        textBtn = (TextView) findViewById(R.id.textBtn);
+
+        bigboss = (ImageView) findViewById(R.id.bigboss);
+        bigboss.startAnimation(smalltobig);
+
+
+        textQuestion.setTypeface(typeface);
+        textScreen.setTypeface(typeface);
+        textTitle.setTypeface(typeface);
+        textBtn.setTypeface(typeface);
+
+    }
+}
